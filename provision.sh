3e10d4b89de67f8d083ac732c4581b4a23cc0af4
Enrique Sanz Yagüe <enriquesanz@gmail.com>
2016-09-13 12:53:58 +0200

    Some customization

--- provision.sh
+++ provision.sh
@@ -2,4 +2,12 @@
 
 git clone https://github.com/SimpaticoUD/interactiveFrontend.git /var/www/public/interactiveFrontend
 cd /var/www/public/interactiveFrontend
-git pull origin master
+
+cd /var/www/public/interactiveFrontend
+if [[ `git status --porcelain` ]]; then
+  # changes
+  git pull origin master
+else
+  # no changes
+  echo "Up to date"
+fi
