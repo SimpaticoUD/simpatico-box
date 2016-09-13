58f917afe2b55f70dddc1fdb13855b9998131ab6
Enrique Sanz Yagüe <enriquesanz@gmail.com>
2016-09-13 12:59:37 +0200

    Added citizenpedia to provision file

--- provision.sh
+++ provision.sh
@@ -3,11 +3,22 @@
 git clone https://github.com/SimpaticoUD/interactiveFrontend.git /var/www/public/interactiveFrontend
 cd /var/www/public/interactiveFrontend
 
-cd /var/www/public/interactiveFrontend
 if [[ `git status --porcelain` ]]; then
   # changes
   git pull origin master
 else
   # no changes
-  echo "Up to date"
+  echo "interactiveFrontend Up to date"
+fi
+
+
+git clone https://github.com/SimpaticoUD/citizenpedia.git /var/www/public/citizenpedia
+cd /var/www/public/citizenpedia
+
+if [[ `git status --porcelain` ]]; then
+  # changes
+  git pull origin master
+else
+  # no changes
+  echo "Citizenpedia Up to date"
 fi
